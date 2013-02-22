56e73ada3d126111d3965aa9ee2c6acab993b0f9
Stephen A. Balaban <stephen@stephenbalaban.com>
2013-02-21 23:59:19 -0800

    added cli.py

---
+++ lmb/cli.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+    cli.py
+    ~~~~~
+
+    :created: 2013-02-21 23:59:15 -0800
+    :copyright: (c) 2013, Lambda Labs, Inc.
+    :license: BSD. See LICENSE.
+"""
+
