62a10ae416920f71a72f32a5e8aecd28cf7ab5be
Stephen A. Balaban <stephen@stephenbalaban.com>
2013-02-21 23:58:10 -0800

    cli.py: python command line tools for lmb

---
+++ lmb/cli.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+    cli.py
+    ~~~~~
+    command line interface
+
+    :created: 2013-02-21 23:39:59 -0800
+    :copyright: (c) 2013, Lambda Labs, Inc.
+    :license: BSD. See LICENSE.
+"""
+
+pass
